35984984be561b799b3b251a5a385919a7ae0e91
brendanobilo <brendan.obilo@dcmail.ca>
2024-07-30 07:55:24 +0100

    Created Arrays and storyText variable

--- js/a4part1.js
+++ js/a4part1.js
@@ -1,4 +1,4 @@
-1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+// 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
 
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
@@ -9,23 +9,17 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-2. RAW TEXT STRINGS
+// 2. RAW TEXT STRINGS
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
 
-It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+let insertx = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 
-Willy the Goblin
-Big Daddy
-Father Christmas
+let inserty = ["the soup kitchen", "Disneyland", "the White House"];
 
-the soup kitchen
-Disneyland
-the White House
+let insertz = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
-spontaneously combusted
-melted into a puddle on the sidewalk
-turned into a slug and crawled away
 
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
 
